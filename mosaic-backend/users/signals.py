7825926001585da40d9e8e762f4aa41844e9d884
Ekaterina Zaharova <ekaterinazaharova62@gmail.com>
2025-12-26 02:34:33 +0400

    feat(backend): create an administrator during migration

--- mosaic-backend/users/signals.py
+++ mosaic-backend/users/signals.py
@@ -2,7 +2,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_migrate
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 
-from users.models import Role
+from users.models import Role, User
 
 
 @receiver(post_migrate)
@@ -22,3 +22,16 @@ def set_online(sender, user, request, **kwargs):
 def set_offline(sender, user, request, **kwargs):
     user.is_online = False
     user.save(update_fields=['is_online'])
+
+@receiver(post_migrate)
+def create_administrator(sender, **kwargs):
+    if sender.name == 'users':
+        role = Role.objects.get(name='Administrator')
+        if User.objects.filter(username='Administrator').exists():
+            print('Administrator already exists')
+        else:
+            User.objects.create_user(
+                username='Administrator',
+                full_name='Administrator',
+                password='Administrator',
+                role=role)
--- mosaic-frontend/src/pages/Admin.tsx
+++ mosaic-frontend/src/pages/Admin.tsx
@@ -3,13 +3,26 @@ import {adminUsers} from '../api/users'
 
 export default function Admin() {
     const [users, setUsers] = useState([])
+    const [access, setAccess] = useState<boolean>(false);
 
     useEffect(() => {
-        adminUsers().then(r => setUsers(r.data))
+        adminUsers().then(r => {
+            if (r.status === 200) {
+                setUsers(r.data)
+            }
+            if (r.status === 403) {
+                setAccess(false)
+            }
+        })
     }, [])
 
     return (
         <ul>
+            {
+                !access && <div>
+                    <h2>Вы должны иметь роль администратора</h2>
+                </div>
+            }
             {users.map((u: any) => (
                 <li key={u.id}>{u.username} | {u.posts_count} | {String(u.is_online)}</li>
             ))}
